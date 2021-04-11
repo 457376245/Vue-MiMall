2235a942c3cf759940f8179c6a95b1575fc7a358
457376245 <457376245@qq.com>
2021-04-11 23:43:03 +0800

    11

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <nav-header></nav-header>
+    <router-view></router-view>
+    <nav-footer></nav-footer>
+  </div>
+</template>
+
+<script>
+import NavFooter from './Nav-Footer.vue'
+import NavHeader from './Nav-Header.vue'
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+    NavHeader,
+    NavFooter
+    
+  }
+}
+</script>
